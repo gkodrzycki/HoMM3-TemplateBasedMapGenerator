8cfe61fd533dc08e1ca7a38d41071e8781817e3f
Grzegorz Kodrzycki <g.kodrzycki20@gmail.com>
2026-01-11 19:24:17 +0100

    Alternative approach

--- generator/mapInfo/Tile.hpp
+++ generator/mapInfo/Tile.hpp
@@ -3,23 +3,32 @@
 #include "../global/Global.hpp"
 #include "./Road.hpp"
 
+enum class TileType { TILE_FREE = 0, TILE_TAKEN };
+
 class Tile {
   public:
     Tile();
     Tile(int zoneID, string terrain);
+    Tile(int zoneID, string terrain, TileType tileType);
 
     void setZoneID(int zoneID);
     void setTerrain(string terrain);
     void setRoad(shared_ptr<Road> road);
+    void setTileType(TileType tileType);
 
     int getZoneID();
     string getTerrain();
     shared_ptr<Road> getRoad();
+    TileType getTileType();
     void printTile();
+
+    string tileTypeToString(TileType tileType);
+    TileType stringToTileType(string tileType);
 
   private:
     int zoneID;
     string terrain;
+    TileType tileType;
 
     shared_ptr<Road> road;
 };
